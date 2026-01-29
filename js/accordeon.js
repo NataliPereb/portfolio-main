5a1e38659bbcb9fba470ae7bea5491d1cd44bc5e
Natali Perebeynos <perebeynos-98@mail.ru>
2026-01-29 11:08:34 +0300

    Задание 2

---
+++ js/accordeon.js
@@ -0,0 +1,14 @@
+const accordeon = document.querySelector('.feature-list');
+const accordeonButton = accordeon.querySelectorAll('.feature__link');
+
+accordeonButton.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        accordeonButton.forEach((button) => {
+            button.classList.remove('feature__link_active');
+            button.nextElementSibling.classList.add('hidden');
+        })
+        
+        btn.classList.toggle('feature__link_active');
+        btn.nextElementSibling.classList.toggle('hidden');
+    })
+})
